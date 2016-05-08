4f5faf8ee5630fecdae989041c1890096473552e
Hancock Xu <Xuhanyu72@gmail.com>
2016-05-08 17:47:52 +0800

    Fix the navigation bar button tint color error
    Add the doc.min.css and patch.css file.
    
    The navigation bar still have problem:
    1. Auto expend after refresh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,13 @@
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<title>@yield('title','FancyGo')</title>
-		<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<style class="anchorjs"></style>
+		<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<link rel="stylesheet" href="data:text/css;charset=utf-8," data-href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" id="bs-theme-stylesheet">
-		<link rel="stylesheet" href="../assets/css/patch.css" type="text/css">
-		<link rel="stylesheet" href="../assets/css/docs.min.css" type="text/css">
-		<script src="../assets/js/ie-emulation-modes-warning.js"></script>
+		<link href="//cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="../packages/patch.css">
+		<link rel="stylesheet" href="../packages/doc.min.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximun-scale=1, user-scalable=no">
-
 	</head>
 
 	<body>
@@ -21,7 +21,9 @@
 			</div>
 		</a>
 
-		@include('site.header')
+		<div class="container">
+			@include('site.header')
+		</div>
 
 		<div class="container">
 			<section class="content">
