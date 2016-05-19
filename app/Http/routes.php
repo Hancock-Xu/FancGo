d4ab33da3820f13d74769d52d93ba350a04e1708
Hancock Xu <Xuhanyu72@gmail.com>
2016-05-19 14:17:30 +0800

    Correct the job controller to right namespace(JobSiteController=>JobController) Add register section in navigation bar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,13 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/about', 'BasicSiteInfoController@about');
 
-Route::get('/jobs','JobSiteController@index');
-
-Route::get('/jobs/create','JobSiteController@create');
-Route::get('/jobs/{id}','JobSiteController@showJobById');
 Route::get('/', 'BasicSiteInfoController@index');
-	Route::post('/jobs/store','JobSiteController@store');
+//
+//Route::get('/jobs','JobSiteController@index');
+//Route::get('/jobs/create','JobSiteController@create');
+//Route::get('/jobs/{id}','JobSiteController@showJobById');
+//Route::post('/jobs/store','JobSiteController@store');
 //login
+
+
 
 Route::get('/auth/login','Auth\AuthController@getLogin');
 Route::post('/auth/login','Auth\AuthController@postLogin');
@@ -36,14 +38,15 @@ Route::get('admin',function(){
 	return redirect('/admin/jobs');
 });
 //
-//Route::group([
-//	'namespace'=>'Admin',
-//	'middleware'=>'auth'], function(){
-//	Route::resource('admin/jobs','JobController');
-//	Route::get('/jobs','JobController@index');
-//	Route::get('/jobs/create','JobSiteController@create');
-////
-//	Route::get('/jobs/{id}', 'JobSiteController@showJobById');
-//	Route::post('/jobs/store','JobSiteController@store');
-//	Route::get('/auth/upload','UploadController@index');
-//});
+Route::group([
+	'namespace'=>'Admin',
+	'middleware'=>'auth'], function(){
+	Route::resource('admin/jobs','JobController');
+	Route::get('/jobs','JobController@index');
+	Route::get('/jobs/create','JobController@create');
+
+
+	Route::get('/jobs/{id}', 'JobController@showJobById');
+	Route::post('/jobs/store','JobController@store');
+	Route::get('/auth/upload','UploadController@index');
+});
--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -20,6 +20,7 @@
 
 	@if(Auth::guest())
 		<li><a href="/auth/login">Login</a></li>
+		<li><a href="/auth/register">Register</a></li>
 	@else
 		<li class="dropdown">
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
