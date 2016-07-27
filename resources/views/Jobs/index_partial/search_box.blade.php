439430c9f8a13d07773f93b03e3f0c23f75bbe2d
Hancock Xu <Xuhanyu72@gmail.com>
2016-07-28 02:34:02 +0800

    Finish searchbar

---
+++ resources/views/Jobs/index_partial/search_box.blade.php
@@ -0,0 +1,310 @@
+<div class="searchbar">
+	<form class="searchbarForm" action="job" method="GET">
+		<div class="searchbarWapper">
+
+			<div class="searchbarCitySelect">
+				<select class="cityselect" name="City">
+
+					<option value="Shenzhen" selected>Shenzhen</option>
+					<option value="Shanghai">Shanghai</option>
+					<option value="Guangzhou">Guangzhou</option>
+					<option value="Beijing">Beijing</option>
+					<option value="Others">Others</option>
+				</select>
+				<i id="cityselecticon" class="glyphicon glyphicon-chevron-down"></i>
+			</div>
+
+			<div class="searchbarJobTypeSelect">
+				<select class="jobtypeselect">
+					<option value="full-time" selected>Full-time</option>
+					<option value="part-time">Part-time</option>
+					<option value="intership">Intership</option>
+				</select>
+				<i id="jobtypeselecticon" class="glyphicon glyphicon-chevron-down"></i>
+			</div>
+
+			<div class="searchbarIndustrySelect">
+
+				<div class="industrywarpper">
+					<div class="industryCell">
+						<label class="industryinputlabel" for="">
+							<!--<span class="inputplaceholderlabel">Industry</span>-->
+							<input type="text" class="industryInput input-large" name="selectedIndustry" id="selectedIndustry" value="" placeholder="Industry" autocomplete="off" readonly>
+							<i id="industryselecticon" class="glyphicon glyphicon-chevron-down"></i>
+							<!--<span class="caret"></span>-->
+						</label>
+
+					</div>
+
+					<div class="industrylist">
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Sales/Marketing
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Sales/Retail</a>
+									</li>
+									<li>
+										<a href="#">Marketing</a>
+									</li>
+									<li>
+										<a href="#">Advertisement </a>
+									</li>
+									<li>
+										<a href="#">PR</a>
+									</li>
+									<li>
+										<a href="#">Customer Service</a>
+									</li>
+								</ul>
+
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Education/Training
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Teaching</a>
+									</li>
+									<li>
+										<a href="#">Translation/Proofreading </a>
+									</li>
+									<li>
+										<a href="#">Other Teaching</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Creative
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Graphic Design</a>
+									</li>
+									<li>
+										<a href="#">Photographer</a>
+									</li>
+									<li>
+										<a href="#">Artwork Designer</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Trade/Logistic
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Sourcing/Purchasing</a>
+									</li>
+									<li>
+										<a href="#">International Trade</a>
+									</li>
+									<li>
+										<a href="#">Logistic</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Manufacture
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Architecture</a>
+									</li>
+									<li>
+										<a href="#">Manufacturing/Production</a>
+									</li>
+									<li>
+										<a href="#">Engineer</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Finance/Consultancy
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Finance</a>
+									</li>
+									<li>
+										<a href="#">Accounting</a>
+									</li>
+									<li>
+										<a href="#">Banking</a>
+									</li>
+									<li>
+										<a href="#">Consultancy</a>
+									</li>
+									<li>
+										<a href="#">Legal</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								IT
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Software Engineer</a>
+									</li>
+									<li>
+										<a href="#">Data Analyst</a>
+									</li>
+									<li>
+										<a href="#">Product Designer</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Admin
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Professional Manager</a>
+									</li>
+									<li>
+										<a href="#">Secretarial/Office manager</a>
+									</li>
+									<li>
+										<a href="#">Human Resource</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Entertainment/Catering
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Acting/Model/Voice</a>
+									</li>
+									<li>
+										<a href="#">Bar/Club/Restaurant Staff</a>
+									</li>
+									<li>
+										<a href="#">Hotel/tourism</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Healthcare
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Physician Job</a>
+									</li>
+									<li>
+										<a href="#">Medical assistant job</a>
+									</li>
+									<li>
+										<a href="#">Biomedical Engineer Job</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="industrylist_row">
+							<strong class="industrylist_row_name">
+								Others
+							</strong>
+							<div class="industrylist_row_content">
+
+								<ul class="industrylist_row_1">
+									<li>
+										<a href="#">Others</a>
+									</li>
+
+								</ul>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+
+			</div>
+
+			<div class="searchbarSalarySelect">
+				<select class="salaryselect">
+					<option value="1">Under 8K/MTH</option>
+					<option value="2">8K-10KMTH</option>
+					<option value="3">10K-15K/MTH</option>
+					<option value="4" selected>15K-20K/MTH</option>
+					<option value="5">20K-30K/MTK</option>
+					<option value="6">30K-50K/MTH</option>
+					<option value="7">Above 50K/MTH</option>
+				</select>
+				<i id="salaryselecticon" class="glyphicon glyphicon-chevron-down"></i>
+			</div>
+
+			<div class="searchbarCompanyNameInput">
+
+				<div class="industryCell">
+					<label class="companyNameInputLabel" for="">
+
+						<input type="text" class="companyNameInput input-large" name="companyname" id="companyname" value="" placeholder="Company name" autocomplete="">
+
+					</label>
+				</div>
+
+			</div>
+
+		</div>
+		<div class="searchbarSearchButton">
+			<button class="searchbarButton" type="submit">
+				Search
+			</button>
+		</div>
+	</form>
+</div>
