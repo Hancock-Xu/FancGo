aa1f1b77f0062898e14da26be1c123a517be0ef2
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-08 12:46:06 +0800

    创建“验证邮件已发送”视图,和相关的css文件

--- resources/views/Company/resend_verify_email.blade.php
+++ resources/views/Company/resend_verify_email.blade.php
@@ -7,15 +7,20 @@
 			<embed src="{{asset('images/no_result.svg')}}" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" />
 		</div>
 		<div class="content-noresult">
-			<h1>You have not yet verified~~ </h1>
+			<h1>You have not verified yet~~ </h1>
 			<h2>Do you need resend verified email to {{$company->company_email}}?</h2>
+
 			<div class="resend_verify_email">
-				<a href="">
+				<form action="{{action('Admin\CompanyController@resendVerifyLinkEmail')}}" method="post">
+					{{--<input type="hidden" name="_method" value="">--}}
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
 					<button type="submit" class="btn btn-primary">
-						<i class="fa fa-btn fa-envelope"></i> Send Verify Link
+						<i class="fa fa-btn fa-envelope"></i> Resend Verify Link
 					</button>
-				</a>
+				</form>
 			</div>
+
 			<div class="Re_create_company">
 				<form action="{{action('Admin\CompanyController@destroy', ['id'=>$company->id])}}" method="post">
 					<input type="hidden" name="_method" value="DELETE">
