399ef46094f8ef1c71dfcd2451e0214bda04f881
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-09 23:46:28 +0800

    在create job 页面中添加Job industry选择框

--- resources/views/Jobs/create.blade.php
+++ resources/views/Jobs/create.blade.php
@@ -84,16 +84,266 @@
 
 						<div class="job_property_part1_right_part">
 
-							<div class="job_industry form-group">
-								<label class="job_label" for="position_industry">Position Industry</label>
-								<i class="glyphicon glyphicon-asterisk required-item"></i>
-								<select name="job_industry" class="form-control job_select" data-style=" btn-success " id="position-industry ">
-									<i class="glyphicon glyphicon-menu-down " ></i>
-									<option>Education</option>
-									<option>Banking</option>
-								</select>
-								{{--<i class="glyphicon glyphicon-menu-down arrow-down "></i>--}}
+							<label class="job_label" for="position_industry">Position Industry</label>
+							<i class="glyphicon glyphicon-asterisk required-item"></i>
+							<div class="job_industry form-group{{ $errors->has('company_industry') ? ' has-error' : '' }}">
+
+									<input type="text" class="industryInput input-large form-control" name="company_industry" id="selectedIndustry" value="" placeholder="Industry" autocomplete="off" readonly>
+
+									@if ($errors->has('company_industry'))
+										<span class="help-block">
+	                                        <strong>{{ $errors->first('company_industry') }}</strong>
+	                                    </span>
+									@endif
+
+								<div class="industrylist">
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Sales/Marketing
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Sales/Retail</a>
+												</li>
+												<li>
+													<a href="#">Marketing</a>
+												</li>
+												<li>
+													<a href="#">Advertisement </a>
+												</li>
+												<li>
+													<a href="#">PR</a>
+												</li>
+												<li>
+													<a href="#">Customer Service</a>
+												</li>
+											</ul>
+
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Education/Training
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Teaching</a>
+												</li>
+												<li>
+													<a href="#">Translation/Proofreading </a>
+												</li>
+												<li>
+													<a href="#">Other Teaching</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Creative
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Graphic Design</a>
+												</li>
+												<li>
+													<a href="#">Photographer</a>
+												</li>
+												<li>
+													<a href="#">Artwork Designer</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Trade/Logistic
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Sourcing/Purchasing</a>
+												</li>
+												<li>
+													<a href="#">International Trade</a>
+												</li>
+												<li>
+													<a href="#">Logistic</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Manufacture
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Architecture</a>
+												</li>
+												<li>
+													<a href="#">Manufacturing/Production</a>
+												</li>
+												<li>
+													<a href="#">Engineer</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Finance/Consultancy
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Finance</a>
+												</li>
+												<li>
+													<a href="#">Accounting</a>
+												</li>
+												<li>
+													<a href="#">Banking</a>
+												</li>
+												<li>
+													<a href="#">Consultancy</a>
+												</li>
+												<li>
+													<a href="#">Legal</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											IT
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Software Engineer</a>
+												</li>
+												<li>
+													<a href="#">Data Analyst</a>
+												</li>
+												<li>
+													<a href="#">Product Designer</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Admin
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Professional Manager</a>
+												</li>
+												<li>
+													<a href="#">Secretarial/Office manager</a>
+												</li>
+												<li>
+													<a href="#">Human Resource</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Entertainment/Catering
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Acting/Model/Voice</a>
+												</li>
+												<li>
+													<a href="#">Bar/Club/Restaurant Staff</a>
+												</li>
+												<li>
+													<a href="#">Hotel/tourism</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Healthcare
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Physician Job</a>
+												</li>
+												<li>
+													<a href="#">Medical assistant job</a>
+												</li>
+												<li>
+													<a href="#">Biomedical Engineer Job</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="industrylist_row">
+										<strong class="industrylist_row_name">
+											Others
+										</strong>
+										<div class="industrylist_row_content">
+
+											<ul class="industrylist_row_1">
+												<li>
+													<a href="#">Others</a>
+												</li>
+
+											</ul>
+										</div>
+									</div>
+
+								</div>
+
 							</div>
+
+
+						{{--<div class="job_industry form-group">--}}
+								{{--<label class="job_label" for="position_industry">Position Industry</label>--}}
+								{{--<i class="glyphicon glyphicon-asterisk required-item"></i>--}}
+								{{--<select name="job_industry" class="form-control job_select" data-style=" btn-success " id="position-industry ">--}}
+									{{--<i class="glyphicon glyphicon-menu-down " ></i>--}}
+									{{--<option>Education</option>--}}
+									{{--<option>Banking</option>--}}
+								{{--</select>--}}
+								{{--<i class="glyphicon glyphicon-menu-down arrow-down "></i>--}}
+							{{--</div>--}}
 							<!-- Employment Type and Expereince -->
 
 
