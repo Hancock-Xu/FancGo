b7510c644f2bc2c30303b0a4490c579ee08c4336
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-11 00:35:24 +0800

    新增jobcontroller中show方法的查询内容

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -135,7 +135,7 @@ class JobController extends Controller
 		$job = \DB::table('jobs')
 			->where('jobs.id','=', $id)
 			->join('companies', 'jobs.company_id', '=', 'companies.id')
-			->select('jobs.*', 'companies.user_id', 'companies.company_name','companies.business_license_name','companies.logo_url','companies.website','companies.company_description','companies.scale','companies.company_location','companies.company_industry','companies.company_email','companies.company_phone_number')
+			->select('jobs.*', 'companies.user_id', 'companies.company_name','companies.business_license_name','companies.logo_url','companies.website','companies.company_description','companies.scale','companies.company_location','companies.company_industry','companies.company_email','companies.company_phone_number', 'companies.founder_time', 'companies.company_address')
 			->first();
 
 		$user = \Auth::getUser();
