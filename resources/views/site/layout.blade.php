d72a624d8fcc07b877e208476a1071f0b72cffef
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-18 11:41:00 +0800

    完成页面footer

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -8,7 +8,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximun-scale=1, user-scalable=no">
 	<meta name="keyword" content="JobLead China, Joblead, Job in China, Job in Shenzhen, Working in China, Life in China, Living in China, Work Visa in China">
 	<meta name="description" content="JobLead China provide a set of job solution for enterises and forein talents.">
-
+	<link rel="stylesheet" href="">
+	<link rel="shortcut icon" type="image/x-icon">
+	<link rel="icon" type="image/x-icon">
 	<title>JobLeadChina </title>
 
 	<script>
@@ -31,9 +33,26 @@
 
 <body>
 
-@include('site.navbar')
+<div class="container" style="min-height: 100%; width: 100%">
 
-@yield('content')
+	<div class="container" style="width: 100%;">
+		@include('site.navbar')
+	</div>
+
+	<div class="jb_content">
+		@yield('content')
+	</div>
+
+</div>
+
+<div id="footer">
+	<div class="container ">
+		<h3>Joblead China</h3>
+		<p>A set of job solution for enterprises and forein talents.</p>
+		<span class="more-about"><a  href="#">More about us</a></span>
+		<span class="contact-email"><a href="mailto:service@jobleadchina.com?subject=The%20subject%20of%20the%20mail">Email:&nbsp;service@jobleadchina.com</a> We will replay you in one day</span>
+	</div>
+</div>
 
 <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
