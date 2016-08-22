f47f8f4c1fb77c66925f0e48a0c647d2ffbd600a
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-22 14:16:10 +0800

    Revert "update composer.json"
    This reverts commit 16a36b376a60c8057561979d50e93a006d7bbadc.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,12 +12,12 @@
 */
 
 Route::get('/', 'Admin\BasicSiteInfoController@index');
-//Route::get('/about', 'BasicSiteInfoController@about');
+Route::get('/about', 'BasicSiteInfoController@about');
 
 Route::get('/auth/login','Auth\AuthController@getLogin');
 Route::post('/auth/login','Auth\AuthController@postLogin');
 Route::get('/auth/logout','Auth\AuthController@getLogout');
-
+//
 Route::get('/auth/register','Auth\AuthController@getRegister');
 Route::post('/auth/register','Auth\AuthController@postRegister');
 
