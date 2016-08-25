68cc14dad5a592d66ee4ee57067b21e93576e36d
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-26 00:58:47 +0800

    在firefox中显示date picker

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -13,6 +13,15 @@
 	<link rel="icon" type="image/x-icon">
 	<title>JobLeadChina </title>
 
+	<script src="http://cdn.jsdelivr.net/webshim/1.12.4/extras/modernizr-custom.js"></script>
+	<!-- polyfiller file to detect and load polyfills -->
+	<script src="http://cdn.jsdelivr.net/webshim/1.12.4/polyfiller.js"></script>
+	<script>
+		webshims.setOptions('waitReady', false);
+		webshims.setOptions('forms-ext', {types: 'date'});
+		webshims.polyfill('forms forms-ext');
+	</script>
+
 	<script>
 		var _hmt = _hmt || [];
 		(function() {
