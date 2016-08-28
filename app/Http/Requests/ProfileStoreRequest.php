40f5e79d2cccc3e43ca4a0711c72a57fb26dc5d6
Hancock Xu <Xuhanyu72@gmail.com>
2016-08-28 13:32:59 +0800

    添加profile store request验证

---
+++ app/Http/Requests/ProfileStoreRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+class ProfileStoreRequest extends Request
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'last_name' => 'required|max:50',
+			'first_name' => 'required|max:50',
+			'phone_number'=>'required',
+			'sex'=>'required',
+			'date_of_birth'=>'required',
+			'nationality'=>'required',
+			'native_language'=>'required',
+			'chinese_level'=>'required',
+			'current_residence'=>'required',
+			'resume_url'=>'required'
+		];
+	}
+
+	public function messages()
+	{
+		return [
+			'last_name.required' => '需要填写姓',
+			'last_name.required' => '需要填写名',
+			'phone_number.required' => '需要填写电话号码',
+			'sex.required' => '需要填写性别',
+			'date_of_birth.required' => '需要填写生日',
+			'nationality.required' => '需要填写国籍',
+			'resume_url.required' => '需要上传简历',
+			'resume_url.mimes:pdf' => '简历格式需要是PDF',
+			'native_language.required' => '需要填写您的母语',
+			'chinese_level.required' => '需要填写中文水平',
+			'current_residence.required' => '需要填写现居地'
+		];
+	}
+
+}
