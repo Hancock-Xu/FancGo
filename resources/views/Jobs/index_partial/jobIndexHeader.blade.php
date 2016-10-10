3fd4fbd93beb22798259c67fd11a855c3243861b
Hancock Xu <Xuhanyu72@gmail.com>
2016-10-10 12:32:30 +0800

    Fix job index header img url

--- resources/views/Jobs/index_partial/jobIndexHeader.blade.php
+++ resources/views/Jobs/index_partial/jobIndexHeader.blade.php
@@ -1,4 +1,4 @@
-<div class="jumbotron" style="background-image: url(http://localhost:8000/uploads/index_img.png); background-attachment:fixed;;background-position: 100% 100%;width: 100%; height: 300px;">
+<div class="jumbotron" style="background-image: url(http://www.jobleadchina.com/uploads/index_img.png); background-attachment:fixed;;background-position: 100% 100%;width: 100%; height: 300px;">
 	{{--<img src="http://localhost:8000/uploads/index_img.png" alt="">--}}
 	<h1 style="color: #fefefe;margin-left: 30px;width: 600px;">Post Job For Free!</h1>
 	<p style="color: #fefefe;margin-left: 30px;width: 600px;padding-top: 20px;">Make recruiting foreign talents more easier</p>
