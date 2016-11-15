e88481c06c3be05cd26e4e75c9df229536f26369
Hancock Xu <Xuhanyu72@gmail.com>
2016-11-15 14:46:18 +0800

    Remove the useless words in website description

--- resources/views/Jobs/index_partial/jobIndexHeader.blade.php
+++ resources/views/Jobs/index_partial/jobIndexHeader.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron" style="background-image: url(http://www.jobleadchina.com/uploads/index_img.jpg); background-attachment:fixed;background-position:top center;background-repeat:repeat; width: 100%; height: 350px; margin-bottom: 50px">
 	{{--<img src="http://localhost:8000/uploads/index_img.png" alt="">--}}
-	<h1 style="color: #fefefe;margin-left: 70px;">The Most Popular Foreign Talents Recruitment Platform</h1>
+	<h1 style="color: #fefefe;margin-left: 70px;">The Most Popular Recruitment Platform For  Foreign Talents</h1>
 	<p style="color: #fefefe;margin-left: 70px;width: 600px;padding-top: 40px;">Make recruiting foreign talents more easier</p>
 	{{--<p style="color: #fefefe;margin: 0 auto;width: 100px;"><a class="btn btn-primary" href="#" role="button">Learn more</a></p>--}}
 </div>
