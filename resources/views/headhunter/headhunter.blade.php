2f1b1be1abe73a9064f03d674853f9645663ffa9
Hancock Xu <Xuhanyu72@gmail.com>
2017-03-16 18:00:12 +0800

    去除 experience 背景图片

--- resources/views/headhunter/headhunter.blade.php
+++ resources/views/headhunter/headhunter.blade.php
@@ -65,7 +65,10 @@
 
 		{{--<h2 style="padding-top: 14px; padding-left:  14px; color: #5b6b7b; margin: 0px">合作商代表</h2>--}}
 
+		<h2 class="color-line" style="margin-left: 20px;">合作企业:</h2>
+
 		<div class="partnersLogoCollectionContainer">
+
 			<div class="partnerLogo">
 				<figure class="partnerLogoFigure">
 					<img class="partnerLogoFigureImg" src="/uploads/NetEaseGames.png" alt="">
@@ -109,7 +112,10 @@
 	</div>
 
 	<div class="experience">
-		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/fengche.jpg);">
+
+		<h2 class="color-line" style="margin-left: 20px">成功案例:</h2>
+
+		<div class="experienceContainer">
 			<div class="specificExperience-sany">
 
 				<div class="specificExperiencePrincipal">
@@ -208,8 +214,109 @@
 
 		</div>
 
-		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/shejitu.jpg);">
+		<div class="experienceContainer">
 			<div class="specificExperience-celec">
+
+				<div class="specificExperience_desc">
+					<h4 class="color-line ">项目详情:</h4>
+					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
+					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
+					<p>
+						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
+					</p>
+				</div>
+
+				<div class="specificExperiencePrincipal">
+					<h4 class="color-line ">项目负责人:</h4>
+
+					<div class="principalBoard">
+
+						<div class="principalAvatar">
+							<figure class="avatar">
+								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
+							</figure>
+
+							<label class="principalName">Ivy</label>
+						</div>
+
+						<div class="company_basic_info">
+							<ul class="company-info-content ">
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Industry</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Founded</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Size</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Address</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxxxxx</label>
+
+										</div>
+									</div>
+
+
+								</li>
+							</ul>
+						</div>
+
+					</div>
+
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="experienceContainer">
+			<div class="specificExperience-netease">
 
 				<div class="specificExperiencePrincipal">
 					<h4 class="color-line ">项目负责人:</h4>
@@ -307,105 +414,11 @@
 
 		</div>
 
-		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/shejitu.jpg);">
-			<div class="specificExperience-celec">
-
-				<div class="specificExperiencePrincipal">
-					<h4 class="color-line ">项目负责人:</h4>
-
-					<div class="principalBoard">
-
-						<div class="principalAvatar">
-							<figure class="avatar">
-								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
-							</figure>
-
-							<label class="principalName">Ivy</label>
-						</div>
-
-						<div class="company_basic_info">
-							<ul class="company-info-content ">
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Industry</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Founded</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Size</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
 
 
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Address</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
+	</div>
 
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxxxxx</label>
-
-										</div>
-									</div>
-
-
-								</li>
-							</ul>
-						</div>
-
-					</div>
-
-
-
-				</div>
-				<div class="specificExperience_desc">
-					<h4 class="color-line ">项目详情:</h4>
-					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
-					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
-					<p>
-						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
-					</p>
-				</div>
-			</div>
-
-		</div>
-
+	<div class="material">
 
 	</div>
 
