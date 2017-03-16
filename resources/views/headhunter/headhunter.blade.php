c053ed61261998b50bf111df301e66ffd7a3f6d7
Hancock Xu <Xuhanyu72@gmail.com>
2017-03-16 16:11:36 +0800

    项目经验介绍部分采用模糊分层的方式

--- resources/views/headhunter/headhunter.blade.php
+++ resources/views/headhunter/headhunter.blade.php
@@ -57,66 +57,147 @@
 
 	</div>
 
-	<div class="slogan" style="width: 100%; background-color: #004771; box-shadow: inset 0px 15px 10px -15px #000;">
+	<div class="slogan">
 		<h2 style="padding: 20px 20px; width: 500px; margin: 0 auto; color: #b5e3fe"> JobLeadChina 是最高效的外籍人才招聘猎头</h2>
 	</div>
 
-	<div class="partnersLogoCollection" style="width: 100%; background-color: #FFFFFF; padding-top: 10px; padding-bottom: 10px; padding-left: 70px; padding-right: 70px">
+	<div class="partnersLogoCollection">
 
 		{{--<h2 style="padding-top: 14px; padding-left:  14px; color: #5b6b7b; margin: 0px">合作商代表</h2>--}}
 
-		<div style="display: flex">
-			<div class="partnerLogo" style="display: inline-block; width: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 100%; height: 100%;" src="/uploads/NetEaseGames.png" alt="">
+		<div class="partnersLogoCollectionContainer">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" src="/uploads/NetEaseGames.png" alt="">
 				</figure>
 			</div>
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 60px; height: 100%;" src="/uploads/SANYZG.png" alt="">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" style="width: 50px;" src="/uploads/SANYZG.png" alt="">
 
 				</figure>
 			</div>
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 100%; height: 100%;" src="/uploads/SFExpress.svg.png" alt="">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" src="/uploads/SFExpress.svg.png" alt="">
 				</figure>
 			</div>
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 100%; height: 100%;" src="/uploads/shclearing.png" alt="">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" src="/uploads/shclearing.png" alt="">
 				</figure>
 			</div>
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 70px; height: 100%;" src="/uploads/TAETEA.jpg" alt="">
-				</figure>
-			</div>
-
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 100%; height: 100%;" src="/uploads/CELEC.jpg" alt="">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" style="width: 70px;" src="/uploads/TAETEA.jpg" alt="">
 				</figure>
 			</div>
 
-			<div class="partnerLogo" style="display: inline-block; width: 100px; height: 100px; margin: 2em; flex: 1; -webkit-box-flex: 1; display: -webkit-box; display: flex; -webkit-box-align: center; align-items: center; position: relative;">
-				<figure style="width: 100%; display: -webkit-box; display: flex; -webkit-box-pack: center; justify-content: center; -webkit-box-align: center; align-items: center; margin: 0 auto;">
-					<img style="width: 100%; height: 100%;" src="/uploads/SANY.png" alt="">
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" src="/uploads/CELEC.jpg" alt="">
+				</figure>
+			</div>
+
+			<div class="partnerLogo">
+				<figure class="partnerLogoFigure">
+					<img class="partnerLogoFigureImg" src="/uploads/SANY.png" alt="">
 				</figure>
 			</div>
 		</div>
 
 	</div>
 
-	<div class="experience" style="">
-		<div class="experienceContainer" style="background-color: #f5f5f5; background-image: url(http://localhost:8000/uploads/fengche.jpg); background-size: 100%; background-position: 25% 35%;">
-			<div class="specificExperience">
-				<div class="specificExperience_image" style="flex: 1; padding: 20px; margin-left: 20px; margin-right: 20px;">
-					<figure>
-						{{--<img style="width: 100%;" src="/uploads/fengche.jpg" alt="">--}}
-					</figure>
+	<div class="experience">
+		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/fengche.jpg);">
+			<div class="specificExperience-sany">
+
+				<div class="specificExperiencePrincipal">
+					<h4 class="color-line ">项目负责人:</h4>
+
+					<div class="principalBoard">
+
+						<div class="principalAvatar">
+							<figure class="avatar">
+								<img class="avatarImg" src="uploads/hancock.jpg" title="Hancock" alt="avatar">
+							</figure>
+
+							<label class="principalName">Hancock</label>
+						</div>
+
+						<div class="company_basic_info">
+							<ul class="company-info-content ">
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Industry</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Founded</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Size</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Address</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxxxxx</label>
+
+										</div>
+									</div>
+
+
+								</li>
+							</ul>
+						</div>
+
+					</div>
+
+
+
 				</div>
-				<div class="specificExperience_desc" style="flex: 1; padding-left: 20px; margin-left: 20px; margin-right: 20px; padding-right: 20px;">
+				<div class="specificExperience_desc">
+					<h4 class="color-line ">项目详情:</h4>
 					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
 					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
 					<p>
@@ -126,25 +207,205 @@
 			</div>
 
 		</div>
-		<div class="experienceContainer" style="background-color: #ffffff">
-			<div class="specificExperience" style="display: flex; margin: 0 auto; width: 980px; margin-top: 40px; margin-bottom: 40px">
 
-				<div class="specificExperience_desc" style="flex: 1; padding-left: 20px; margin-left: 20px; margin-right: 20px; padding-right: 20px;">
+		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/shejitu.jpg);">
+			<div class="specificExperience-celec">
+
+				<div class="specificExperiencePrincipal">
+					<h4 class="color-line ">项目负责人:</h4>
+
+					<div class="principalBoard">
+
+						<div class="principalAvatar">
+							<figure class="avatar">
+								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
+							</figure>
+
+							<label class="principalName">Ivy</label>
+						</div>
+
+						<div class="company_basic_info">
+							<ul class="company-info-content ">
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Industry</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Founded</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Size</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Address</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxxxxx</label>
+
+										</div>
+									</div>
+
+
+								</li>
+							</ul>
+						</div>
+
+					</div>
+
+
+
+				</div>
+				<div class="specificExperience_desc">
+					<h4 class="color-line ">项目详情:</h4>
 					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
 					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
 					<p>
 						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
 					</p>
 				</div>
-				<div class="specificExperience_image" style="flex: 1; padding: 20px; margin-left: 20px; margin-right: 20px;">
-					<figure>
-						<img style="width: 100%;" src="/uploads/shejitu.jpg" alt="">
-					</figure>
-				</div>
-
 			</div>
 
 		</div>
+
+		<div class="experienceContainer" style="background-image: url(http://localhost:8000/uploads/shejitu.jpg);">
+			<div class="specificExperience-celec">
+
+				<div class="specificExperiencePrincipal">
+					<h4 class="color-line ">项目负责人:</h4>
+
+					<div class="principalBoard">
+
+						<div class="principalAvatar">
+							<figure class="avatar">
+								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
+							</figure>
+
+							<label class="principalName">Ivy</label>
+						</div>
+
+						<div class="company_basic_info">
+							<ul class="company-info-content ">
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Industry</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Founded</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Size</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxx</label>
+
+										</div>
+									</div>
+								</li>
+
+
+								<li>
+									<div class="basic_info_container">
+										<div class="basic_info_item">
+											<span class="color-line">Address</span>
+											{{--<label for="">Industry</label>--}}
+										</div>
+
+										<div class="basic_info_content">
+
+											<label class="basic_info_label">xxxxxxxxxxxx</label>
+
+										</div>
+									</div>
+
+
+								</li>
+							</ul>
+						</div>
+
+					</div>
+
+
+
+				</div>
+				<div class="specificExperience_desc">
+					<h4 class="color-line ">项目详情:</h4>
+					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
+					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
+					<p>
+						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
+					</p>
+				</div>
+			</div>
+
+		</div>
+
 
 	</div>
 
