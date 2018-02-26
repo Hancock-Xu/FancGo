a845e36bcdc432f621548e90fcdfc7d6e87423ee
Hancock <hancock@jobleadchina.com>
2018-02-26 11:24:38 +0800

    完成合作伙伴logo排版，和其他css位置微调

--- resources/views/headhunter/headhunter.blade.php
+++ resources/views/headhunter/headhunter.blade.php
@@ -86,330 +86,71 @@
 
             </div>
 
+		</div>
 
-            {{--<div class="blogThumbnailContainer">--}}
+	</div>
 
-                {{--<div class="blogThumbnail">--}}
-                    {{--<h3 class="blogThumbnailTitle color-line">--}}
-                        {{--猎头在人才招聘中的作用以及如何选择靠谱的猎头公司--}}
-                    {{--</h3>--}}
-                    {{--<figure class="partnerLogoFigure">--}}
-                        {{--<img class="partnerLogoFigureImg" src="http://localhost:8000/images/headhunter/business_building.jpg" alt="">--}}
-                    {{--</figure>--}}
-                    {{--<p class="blogIntroduction">--}}
-                        {{--background-attachment: fixed;--}}
-                        {{--background-position: top center;--}}
-                        {{--background-repeat: repeat;--}}
-                        {{--background-size: contain;    background-attachment: fixed;--}}
-                        {{--background-position: top center;--}}
-                        {{--background-repeat: repeat;--}}
-                        {{--background-size: contain;--}}
-                    {{--</p>--}}
-                {{--</div>--}}
+	<div class="customer-feedback-container">
+		<div class="customer-feedback-wrap">
 
-            {{--</div>--}}
+			<h2 class="customer-feedback"><span>“ </span>So far it has been a great work experience, a lot of fun and my co-workers are very supportive. I am very happy with my move. Thank you for finding me this job, I think I’m fitting in pretty well, Thank you very much for your offer!
+				<span> ”</span></h2>
+			<p class="customer">————某高端外籍人才入职反馈</p>
+
 		</div>
 
 	</div>
 
 	<div class="experience">
 
-		<h2 class="color-line" style="margin-left: 20px">成功案例:</h2>
+		<p class="our-experience">项目案例</p>
 
-		<div class="experienceContainer">
-			<div class="specificExperience-sany">
+		<div class="experience-container">
 
-				<div class="specificExperiencePrincipal">
-					<h4 class="color-line ">项目负责人:</h4>
+			<div class="project-experience">
 
-					<div class="principalBoard">
-
-						<div class="principalAvatar">
-							<figure class="avatar">
-								<img class="avatarImg" src="uploads/hancock.jpg" title="Hancock" alt="avatar">
-							</figure>
-
-							<label class="principalName">Hancock</label>
-						</div>
-
-						<div class="company_basic_info">
-							<ul class="company-info-content ">
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Industry</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Founded</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Size</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Address</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxxxxx</label>
-
-										</div>
-									</div>
-
-
-								</li>
-							</ul>
-						</div>
-
-					</div>
-
-
-
-				</div>
-				<div class="specificExperience_desc">
-					<h4 class="color-line ">项目详情:</h4>
-					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
-					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
-					<p>
-						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
-					</p>
-				</div>
-			</div>
-
-		</div>
-
-		<div class="experienceContainer">
-			<div class="specificExperience-celec">
-
-				<div class="specificExperience_desc">
-					<h4 class="color-line ">项目详情:</h4>
-					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
-					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
-					<p>
-						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
-					</p>
+				<div class="experience-title">
+					<p class="experience-industry-area">建筑工程领域</p>
 				</div>
 
-				<div class="specificExperiencePrincipal">
-					<h4 class="color-line ">项目负责人:</h4>
+				<div class="customer-introduce">某知名景观建筑公司
+					（建筑景观100强）
+				</div>
 
-					<div class="principalBoard">
-
-						<div class="principalAvatar">
-							<figure class="avatar">
-								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
-							</figure>
-
-							<label class="principalName">Ivy</label>
-						</div>
-
-						<div class="company_basic_info">
-							<ul class="company-info-content ">
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Industry</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Founded</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Size</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Address</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxxxxx</label>
-
-										</div>
-									</div>
-
-
-								</li>
-							</ul>
-						</div>
-
-					</div>
-
-
-
+				<div class="experience-detail">
+					<p>在该招聘中，我们2周内推荐面试11位候选人，最终录用2名（资深景观主创设计师、资深景观深化设计师），完成全部招聘指标。</p>
+					<p>其中一名候选人在办签证期间，恰逢中国签证政策调整以及本国海外就业政策变化。数月以来，我们一直跟进协调，帮助处理签证出现的各种意外，最终候选人顺利入职，保障客户利益。</p>
 				</div>
 
 			</div>
 
-		</div>
-
-		<div class="experienceContainer">
-			<div class="specificExperience-netease">
-
-				<div class="specificExperiencePrincipal">
-					<h4 class="color-line ">项目负责人:</h4>
-
-					<div class="principalBoard">
-
-						<div class="principalAvatar">
-							<figure class="avatar">
-								<img class="avatarImg" src="uploads/ivy.jpg" title="Hancock" alt="avatar">
-							</figure>
-
-							<label class="principalName">Ivy</label>
-						</div>
-
-						<div class="company_basic_info">
-							<ul class="company-info-content ">
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Industry</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Founded</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Size</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxx</label>
-
-										</div>
-									</div>
-								</li>
-
-
-								<li>
-									<div class="basic_info_container">
-										<div class="basic_info_item">
-											<span class="color-line">Address</span>
-											{{--<label for="">Industry</label>--}}
-										</div>
-
-										<div class="basic_info_content">
-
-											<label class="basic_info_label">xxxxxxxxxxxx</label>
-
-										</div>
-									</div>
-
-
-								</li>
-							</ul>
-						</div>
-
-					</div>
-
-
-
+			<div class="project-experience">
+				<div class="experience-title">
+					<p class="experience-industry-area">制造业领域</p>
 				</div>
-				<div class="specificExperience_desc">
-					<h4 class="color-line ">项目详情:</h4>
-					<h2>为三一重能筹建西班牙研发中心提供人才保障</h2>
-					<p>三一重能计划在欧洲筹建研发院, 招聘欧洲顶尖的风机领域人才, 以吸收欧洲风电领域先进技术和经验。 研究院主要负责新产品研发、新技术探索并将和中国研究院在技术研发方面进行紧密合作。此时, 人才, 成了筹备研究院最重要的工作。</p>
-					<p>
-						三一重能找到了我们, 我们接到需求之后, 开始紧锣密鼓地成立招聘小组, 调查欧洲风电市场现状和人力资源情况, 开辟新的招聘渠道。最终, 周密的调查准备工作精准的划分出猎寻人群, 找到了准确的方向, 极大的提升了招聘效率。 我们推荐的第一位候选人即为最终录取候选人。
-					</p>
+
+				<div class="customer-introduce">某重型能源装备制造商
+					（世界500强）
+				</div>
+
+				<div class="experience-detail">
+					<p>该公司布局在海外筹建研发中心，该海外研发中心主要为国内研究所起到像老师一样的指导作用，因此招聘需求定位在行业顶尖的研发工程师。</p>
+					<p>我们在接到招聘需求后，成立项目组，在3个月内，推荐了数十位候选人，最终成功为客户引进行业3位业内知名的研发工程师。保障研发中心顺利筹建。</p>
+				</div>
+			</div>
+
+			<div class="project-experience">
+				<div class="experience-title">
+					<p class="experience-industry-area">互联网领域</p>
+				</div>
+
+				<div class="customer-introduce">某知名游戏公司
+					（游戏行业全球前10）
+				</div>
+
+				<div class="experience-detail">
+					<p>该公司近些年来一直布局面向海外市场的游戏研发和发行，同时也为了促进团队国际化。因此需要招聘各个职能的外籍人才。</p>
+					<p>我们成功为该公司招聘到游戏开发，游戏美术（技美，原画，3D，UI 等），游戏策划，游戏运营，本地化等多名外籍人才。其中，游戏美术有多名外籍人才以专家级别待遇从海外引进。</p>
 				</div>
 			</div>
 
@@ -417,45 +158,56 @@
 
 	</div>
 
-	<div class="feature_wrap">
+	<div class="customer-feedback-container">
+		<div class="customer-feedback-wrap">
 
-		<div class="feature_row">
-
-			<div class="feature">
-				
-			</div>
-
-			<div class="feature">
-
-			</div>
+			<h2 class="customer-feedback"><span>“ </span>用一句话说就是高效，跟你们合作的三个多月你们就完成了招聘，保证了我们海外研发中心人才的及时到位。也给了我们很多该岗位上薪酬水平、福利等专业的咨询，很感谢你们。<span> ”</span></h2>
+			<p class="customer">————国内某知名重型能源装备制造公司HR</p>
 
 		</div>
 
-		<div class="feature_row">
+	</div>
 
-			<div class="feature">
+	<div class="features">
 
-			</div>
+		<div class="features-container">
 
-			<div class="feature">
+			<p class="why-choose-us">为什么选择我们</p>
 
-			</div>
+			<div class="features-wrap">
 
-		</div>
+				<div class="features-bag">
 
-		<div class="feature_row">
+					<div class="specificExperiencePrincipal">
 
-			<div class="feature">
+						<h1 class="features-title">行业细分，定位清晰</h1>
+						<p class="feature-detail">专注于专业领域外籍人才招聘，解决企业引进外籍人才招聘痛点</p>
 
-			</div>
+						<h1 class="features-title">信誉优良</h1>
+						<p class="feature-detail">公司90%的业务都来自于长期合作及客户推荐，我们深耕每一个项目，和客户保持长期的合作关系</p>
+						<h1 class="features-title">优秀的顾问团队</h1>
+						<p class="feature-detail">顾问团队英语优秀，和外籍候选人沟通顺畅。丰富的从业经验，善于把握岗位需求，对候选人质量精益求精</p>
+					</div>
 
-			<div class="feature">
+					<div class="specificExperience_desc">
+						<h1 class="features-title">善于攻坚难度大的岗位</h1>
+						<p class="feature-detail">我们采用高底薪低提成的激励机制，顾问团队专业稳定。能自始自终长期跟进完成招聘项目</p>
+						<h1 class="features-title">提供优质的附加服务</h1>
+						<p class="feature-detail">我们为企业提供薪酬谈判和行业薪酬报告，帮助企业制定出合理的薪资水平</p>
+						<h1 class="features-title">提供长达半年的保证期</h1>
+						<p class="feature-detail">为企业提供长达6个月的保证期，候选人保证期内离职，我们免费提供候补候选人</p>
+					</div>
+
+				</div>
+
+
 
 			</div>
 
 		</div>
 
 	</div>
+
 
 	<div class="customer-feedback-container">
 		<div class="customer-feedback-wrap">
@@ -468,80 +220,90 @@
 	</div>
 
 	<div class="partnersLogoCollection">
-		<h2 class="color-line" style="margin-left: 20px;">合作企业:</h2>
+		{{--<p class="why-choose-us">合作企业</p>--}}
+
+		<p class="why-choose-us">合作企业</p>
 
 		<div class="partnersLogoCollectionContainer">
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/shunfeng.png" alt="">
-				</figure>
-			</div>
+			<div class="partnersLogoGroup">
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img style="height: 80px;" class="partnerLogoFigureImg" src="/images/customers/sany.png" alt="">
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/shunfeng.jpg" alt="">
+					</figure>
+				</div>
 
-				</figure>
-			</div>
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/sany.png" alt="">
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img style="height: 80px" class="partnerLogoFigureImg" src="/images/customers/netease_games.png" alt="">
-				</figure>
-			</div>
+					</figure>
+				</div>
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img style="height: 110px;" class="partnerLogoFigureImg" src="/images/customers/opera.png" alt="">
-				</figure>
-			</div>
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/netease_games.png" alt="">
+					</figure>
+				</div>
 
-
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/tencent.png" alt="">
-				</figure>
-			</div>
-
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img style="height: 80px;" class="partnerLogoFigureImg" src="/images/customers/pactera.png" alt="">
-				</figure>
-			</div>
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/opera.jpg" alt="">
+					</figure>
+				</div>
 
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/360.png" alt="">
-				</figure>
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/tencent.jpg" alt="">
+					</figure>
+				</div>
+
 			</div>
 
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/zhongjia.png" alt="">
-				</figure>
-			</div>
+			<div class="partnersLogoGroup">
+
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/zhongjia.png" alt="">
+					</figure>
+				</div>
+
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/pactera.jpg" alt="">
+					</figure>
+				</div>
+
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/zhongguoshiyou.png" alt="">
+					</figure>
+				</div>
+
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img class="partnerLogoFigureImg" src="/images/customers/celec.jpg" alt="">
+					</figure>
+				</div>
+
+				<div class="partnerLogo">
+					<figure class="partnerLogoFigure">
+						<img style="height: 50px;" class="partnerLogoFigureImg" src="/images/customers/hna.png" alt="">
+					</figure>
+				</div>
+
+				{{--<div class="partnerLogo">--}}
+				{{--<figure class="partnerLogoFigure">--}}
+				{{--<img class="partnerLogoFigureImg" src="/images/customers/360.jpg" alt="">--}}
+				{{--</figure>--}}
+				{{--</div>--}}
 
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/zhongguoshiyou.png" alt="">
-				</figure>
 			</div>
 
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img class="partnerLogoFigureImg" src="/images/customers/celec.png" alt="">
-				</figure>
-			</div>
-
-			<div class="partnerLogo">
-				<figure class="partnerLogoFigure">
-					<img style="height: 65px" class="partnerLogoFigureImg" src="/images/customers/hna.png" alt="">
-				</figure>
-			</div>
 
 		</div>
 
